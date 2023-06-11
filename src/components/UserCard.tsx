6a36dca63cdc8c4aca145f41a257a0fbc92404e7
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-06-11 18:10:53 +0700

    feat: User card into accordion

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,15 +2,13 @@ import { User } from "~/types/index.type";
 
 export default function UserCard({ user }: { user: User }) {
   return (
-    <div className="card bg-neutral">
-      <div className="card-body">
-        <div className="avatar">
-          <div className="w-8 rounded-full">
-            <img src={user.avatar_url} alt={`user ${user.login} profile`} />
-          </div>
-        </div>
-        <h1 className="card-title"> {user.login} </h1>
-
+    <div className="collapse collapse-arrow bg-neutral">
+      <input type="radio" name="user-accordion" />
+      <div className="collapse-title text-lg font-medium">
+        {user.login}
+      </div>
+      <div className="collapse-content">
+        <p>hello</p>
       </div>
     </div>
   )
