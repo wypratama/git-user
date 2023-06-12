e2826ccf076673e27c86c91398c8ed4de579a60c
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-06-13 01:11:47 +0700

    fix: Remove react query codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import App from './App.tsx'
 import './index.css'
-import queryClient from './lib/react-query.ts'
-import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
